5d181f4a3b8a653ca8dde0bbe7edf8516cc7b902
Steffan Crowley <steffancrow@gmail.com>
2022-08-18 14:09:05 -0700

    Initial Commit

---
+++ Conversion-Tool-IOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Conversion-Tool-IOS
+//
+//  Created by steffan crowley on 8/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Conversion-Tool-IOS/Conversion_Tool_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Conversion_Tool_IOSApp.swift
+//  Conversion-Tool-IOS
+//
+//  Created by steffan crowley on 8/18/22.
+//
+
+import SwiftUI
+
+@main
+struct Conversion_Tool_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
